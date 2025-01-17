7f5619c2abc8d2f6c1edb6254b8c2c6556152303
Fery Andika <feryandika151@gmail.com>
2025-01-17 21:40:00 +0700

    feat(cms):crud for booking transaction and api keys

--- README.md
+++ README.md
@@ -1,3 +1,61 @@
-# Filament Setup
-- Email: fery@rentoffice.com
-- Password: 123456789
+# RentOffice Setup Guide  
+
+### Admin Credentials  
+- Email: `fery@rentoffice.com`  
+- Password: `123456789`  
+
+---
+
+### 🚀 Project Steps  
+**1. Install Laravel**
+Run the following command to install Laravel 11:  
+```bash  
+composer create-project laravel/laravel rentoffice
+```
+###
+**2. Configure Environment**
+Update the `.env` file with the following details:  
+- Database Settings: Configure your database credentials (`DB_DATABASE`, `DB_USERNAME`, `DB_PASSWORD`).
+- App URL: Set `APP_URL` to match your application's domain.
+###
+**3. Generate Models and Migrations**
+Run these commands to create models with migrations:  
+```bash  
+php artisan make:model City -m  
+php artisan make:model OfficeSpace -m  
+php artisan make:model OfficeSpacePhoto -m  
+php artisan make:model OfficeSpaceBenefit -m  
+php artisan make:model BookingTransaction -m  
+php artisan make:model ApiKey -m 
+```
+###
+**4. Install Filament Admin Panel**
+Filament is a modern Laravel admin panel.
+Learn more here: [Filament](https://filamentphp.com)
+Use the following commands to install Filament:
+```bash  
+composer require filament/filament:"^3.2" -W  
+php artisan filament:install --panels  
+php artisan make:filament-user
+```
+During installation, you can create your first Filament user for admin access.
+###
+**5. Setting Resource Filament**
+Use the following commands to install Resource Filament:
+``` bash
+php artisan make:filament-resource City
+php artisan make:filament-resource OfficeSpace
+php artisan make:filament-resource BookingTransaction
+php artisan make:filament-resource ApiKey
+```
+###
+**6. Setting Up CMS**
+CRUD Management
+- `City`: Add, edit, and manage data city.
+- `Office Space`: Handle listings for office spaces, including details, photos, and benefits.
+- `Booking Transaction`: Track and manage user bookings for office spaces.
+
+Additional Models
+- `OfficeSpacePhoto`: Manage photos for each office space.
+- `OfficeSpaceBenefit`: Attach benefits like WiFi, parking, and more to office spaces.
+- `ApiKey`: Secure integration with external API.
--- app/Filament/Resources/BookingTransactionResource.php
+++ app/Filament/Resources/BookingTransactionResource.php
@@ -9,6 +9,7 @@ use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -19,22 +20,85 @@ class BookingTransactionResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-credit-card';
 
+    // Untuk membuat form input pada data booking transaction, digunakan saat create dan edit
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                //
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+
+                Forms\Components\TextInput::make('booking_trx_id')
+                    ->required()
+                    ->maxLength(255),
+
+                Forms\Components\TextInput::make('phone_number')
+                    ->required()
+                    ->maxLength(255),
+
+                Forms\Components\TextInput::make('total_amount')
+                    ->required()
+                    ->numeric()
+                    ->prefix('IDR'),
+
+                Forms\Components\TextInput::make('duration')
+                    ->required()
+                    ->numeric()
+                    ->prefix('Days'),
+
+                Forms\Components\DatePicker::make('started_at')
+                    ->required(),
+
+                Forms\Components\DatePicker::make('ended_at')
+                    ->required(),
+
+                Forms\Components\Select::make('is_paid')
+                    ->options([
+                        true => 'Paid',
+                        false => 'Not Paid',
+                    ])
+                    ->required(),
+
+                Forms\Components\Select::make('office_space_id')
+                    ->relationship('officeSpace', 'name')
+                    ->searchable()
+                    ->preload()
+                    ->required(),
             ]);
     }
 
+    // Untuk tampilan data (View)
     public static function table(Table $table): Table
     {
         return $table
+            // Isi data pada table booking transaction dan sesuai yang sudah dibuat pada form sebelumnya
             ->columns([
-                //
+                Tables\Columns\TextColumn::make('booking_trx_id')
+                    ->searchable(),
+
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+
+                Tables\Columns\TextColumn::make('officeSpace.name')
+                    ->searchable(),
+
+                Tables\Columns\TextColumn::make('started_at')
+                    ->date(),
+
+                Tables\Columns\IconColumn::make('is_paid')
+                    ->boolean()
+                    ->trueColor('success')
+                    ->falseColor('danger')
+                    ->trueIcon('heroicon-o-check-circle')
+                    ->falseIcon('heroicon-o-x-circle')
+                    ->label('Sudah Bayar?'),
             ])
+            // Untuk filtering data
             ->filters([
-                //
+                SelectFilter::make('office_space_id')
+                    ->label('Office')
+                    ->relationship('officeSpace', 'name'),
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
