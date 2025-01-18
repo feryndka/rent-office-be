1869af882656206d15fa2f810a6201658a7c6756
Fery Andika <feryandika151@gmail.com>
2025-01-18 19:41:44 +0700

    build(controller):setting controller for each endpoint api

--- app/Http/Controllers/Api/CityController.php
+++ app/Http/Controllers/Api/CityController.php
@@ -3,9 +3,28 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Api\CityResource;
+use App\Models\City;
 use Illuminate\Http\Request;
 
 class CityController extends Controller
 {
-    //
+    public function index()
+    {
+        // Mengambil data city dari model, dan menghitung total office dari method officeSpaces
+        $cities = City::withCount('officeSpaces')->get();
+        // Menggunakan collection karena akan mengambil banyak data city
+        return CityResource::collection($cities);
+    }
+
+    // Untuk detail data city
+    public function show(City $city) // Model Binding
+    {
+        // Mengambil data office yang berada di kota tersebut, dan mengambil photo dari office
+        $city->load(['officeSpaces.city', 'officeSpaces.photos']);
+        // Menghitung data kantor di suatu kota
+        $city->loadCount('officeSpaces');
+        // Menggunakan new karena hanya mengambil 1 data kota
+        return new CityResource($city);
+    }
 }
--- app/Http/Controllers/Api/OfficeSpaceController.php
+++ app/Http/Controllers/Api/OfficeSpaceController.php
@@ -3,9 +3,26 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Api\OfficeSpaceResource;
+use App\Models\OfficeSpace;
 use Illuminate\Http\Request;
 
 class OfficeSpaceController extends Controller
 {
-    //
+    public function index()
+    {
+        // Mengambil data kantor dari model yang memiliki relasi dengan data city
+        $officeSpaces = OfficeSpace::with(['city'])->get();
+        // Menggunakan collection karena akan mengambil banyak data office
+        return OfficeSpaceResource::collection($officeSpaces);
+    }
+
+    // Untuk detail data office
+    public function show(OfficeSpace $officeSpace) // Model Binding
+    {
+        // Mengambil data office dari model, dan mengambil city, photo, serta benefits dari kantor tersebut
+        $officeSpace->load(['city', 'photos', 'benefits']);
+        // Menggunakan new karena hanya mengambil 1 data office
+        return new OfficeSpaceResource($officeSpace);
+    }
 }
